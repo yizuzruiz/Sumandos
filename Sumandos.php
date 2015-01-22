90464d9a2370581ce30d24e7dda14eb80eefb205
Jesús A. Ruiz Moreno <ja.ruiz.moreno@gmail.com>
2015-01-21 21:51:07 -0700

    Subir

---
+++ Sumandos.php
@@ -0,0 +1,49 @@
+<?php 
+
+/*Clase que evalua las celdas y obtiene combinaciones de sumandos de un valor.*/ 
+
+
+class Sumandos{
+
+/*Metodo recursivo que evalua y obtiene las combinaciones*/
+
+	static function assess($curTotal, $bPosicion,$bNumElem,$curObjetivo,$curMejor,$curValores,$valpermitidos) {
+		$contar=1;   
+
+ 		if( $bPosicion < $bNumElem ){
+        	$valpermitidos[$bPosicion] = 0;	
+			Sumandos::assess($curTotal, $bPosicion + 1,$bNumElem,$curObjetivo,$curMejor,$curValores,$valpermitidos);
+        
+			$valpermitidos[$bPosicion] = 1;
+        	Sumandos::assess($curTotal + $curValores[$bPosicion], $bPosicion + 1,$bNumElem,$curObjetivo,$curMejor,$curValores,$valpermitidos);
+    
+		}else{
+		
+			if( (abs($curTotal - $curObjetivo) < abs($curObjetivo - $curMejor)) ){
+            			$n;
+            			$curMejor = $curTotal;
+            		
+				if( $curMejor == $curObjetivo ){
+                		$blEncontrado = true;
+                			for($n = 0; $n < $bNumElem; $n++){
+                 				if($valpermitidos[$n]==1){
+                    					echo $contar."<br>";//Regresa los valores
+                  				}
+				$contar=$contar+1;
+
+               				 }
+               
+                		}	
+            			for($n = 0; $n < $bNumElem; $n++){
+                		//$valpermitidos[$n] = $valpermitidos[$n];
+            			}
+        		}
+   		}
+	} 
+
+
+}
+
+
+
+?>
